83d9dfecba393b0e3dd81ff00f6601f39a84c9fa
Clara <clarasm.20@gmail.com>
2019-07-13 15:53:01 +0200

    Cambie el HTML para la página

--- core/urls.py
+++ core/urls.py
@@ -14,19 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def inicio(request):
-    return HttpResponse("No sé qué poner")
-
-def fin(request):
-    return HttpResponse("Sigo sin saber sé qué poner")
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", inicio), 
-    path("Clara", fin)
+    path('', include('blog.urls')),
+    
 ]
 
