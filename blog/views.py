4bbab5a8b78a4dcc96d4934af674d813d7461aca
Clara <clarasm.20@gmail.com>
2019-07-13 17:46:27 +0200

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
+
 
 def post_list(request):
-    return render (request,"blog/post_list.html")
+    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
+    return render (request,"blog/post_list.html", {"lista":post_list})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 # Create your views here.
